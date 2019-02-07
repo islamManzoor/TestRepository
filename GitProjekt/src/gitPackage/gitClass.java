640c01662e73c47ff194211ca0caeacfea0b6a62
islamManzoor <islamManzoor@users.noreply.github.com>
2019-02-07 12:47:21 +0500

    Commit from Eclipse.

---
+++ GitProjekt/src/gitPackage/gitClass.java
@@ -0,0 +1,13 @@
+package gitPackage;
+
+public class gitClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
